879bab046af0ae336fc707d71d677835953d0ab1
BrookVerse <brookewaldorf90@gmail.com>
2022-10-25 13:23:25 +0100

    added profile styling

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -19,7 +19,7 @@ const Profile = ({ userName, onChange, userImage, handleDelete, userInfo, teamNa
             </label>
             <div className="profile__trainerInfo">
               <h2 className="profile__trainerName">{trainerInfo.trainer_name}</h2>
-              <p className="profile__trainerGame">{trainerInfo.game_info}</p>
+              <p className="profile__trainerNotes">{trainerInfo.game_info}</p>
               <p className="profile__trainerNotes">{trainerInfo.description}</p>
               <div className="profile__team">{pokeTeam}</div>
               <button value={trainerInfo.id} onClick={handleDelete}>
